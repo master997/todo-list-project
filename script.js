76fd6b91a469bd613c9f8fcb86a95910dbea5642
Mohammed Master <mohammedmaster1793@gmail.com>
2024-07-18 08:52:48 +0100

    progress on to do list

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+const taskInput = document.querySelector(".add-task-input");
+const addButton = document.querySelector(".add-task-button");
+const taskList = document.querySelector(".task-list");
+
+function addTask() {
+  console.log("Task has been added");
+}
+addButton.addEventListener("click", addTask);
